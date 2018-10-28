115b185ae91a325636085e22cde9813c4c3c6465
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-10-27 20:57:53 -0500

    Updates

---
+++ precache-manifest.9f74b7720db6dcc37fa7ed93e9a6be3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "db3000449ee3cfd6d8bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/runtime~main.db300044.js"
+  },
+  {
+    "revision": "3722c17efb07655182a0",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.3722c17e.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "3722c17efb07655182a0",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.dbedc0b5.chunk.css"
+  },
+  {
+    "revision": "8941e84cd1749e32482444b36883c98a",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/index.html"
+  }
+];
